ed8e9b3ab77bd365c04ab6f81695f0376e154d80
Eduar Enrique Perez Muñoz <145591018+Eduar-Perez@users.noreply.github.com>
2025-05-30 16:20:28 -0500

    Update playground2.py

--- playground2.py
+++ playground2.py
@@ -77,5 +77,5 @@ if __name__ == "__main__":
     agent = agente_generico(MODELS)
     app = create_api_fastapi_app(agent)
     uvicorn.run(app, host="0.0.0.0", port=8080)
-    main()
+    #main()
     #handler = Mangum(app)
