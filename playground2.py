07151ffbc3dc2a1b915d5d415fd317d0c1daf201
Eduar-Perez <eduarp-741@hotmail.com>
2025-06-16 13:28:43 -0500

    nuevo ajuste

--- playground2.py
+++ playground2.py
@@ -20,7 +20,7 @@ from dotenv import load_dotenv
 # PARA OBTENER SECRETS MANAGER DE AWS
 #load_aws_secrets()
 # PARA OBTENER VATRIABLES DE ENTORNO DE .env
-load_dotenv()
+load_dotenv() 
 
 MODELS = "us.anthropic.claude-3-7-sonnet-20250219-v1:0"
 
