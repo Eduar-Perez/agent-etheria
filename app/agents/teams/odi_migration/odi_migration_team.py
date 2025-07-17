e8b69625c5e07e54b428cb17e631e44af778eff8
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-07-17 09:48:41 -0500

    se añade el modelo de migracion odi

--- app/agents/teams/odi_migration/odi_migration_team.py
+++ app/agents/teams/odi_migration/odi_migration_team.py
@@ -67,7 +67,7 @@ def get_bedrock_llm():
 def user_explanation(technical_explanation: str) -> str:
     # Lee el prompt amigable
     # prompt_path = os.path.join('prompts', 'user_experto_with_multiple_task.txt')
-    prompt_path =  os.path.abspath(os.path.join(os.path.dirname(__file__), 'prompts', 'user_experto_3.txt'))
+    prompt_path = os.path.join(os.path.dirname(__file__), 'prompts', 'user_experto_3.txt')
     with open(prompt_path, "r", encoding="utf-8") as f:
         prompt_template = f.read()
     # prompt = prompt_template.format(technical_explanation=technical_explanation, nombre_proceso_ppal=nombre_proceso_ppal)
