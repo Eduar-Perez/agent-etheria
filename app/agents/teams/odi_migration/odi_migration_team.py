13a91b9fa47ee9364ceca3d6fb6013313bf56287
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-07-17 09:09:50 -0500

    se corrige erores de extraccion de prompts en description_tarea

--- app/agents/teams/odi_migration/odi_migration_team.py
+++ app/agents/teams/odi_migration/odi_migration_team.py
@@ -67,7 +67,7 @@ def get_bedrock_llm():
 def user_explanation(technical_explanation: str) -> str:
     # Lee el prompt amigable
     # prompt_path = os.path.join('prompts', 'user_experto_with_multiple_task.txt')
-    prompt_path = os.path.join("..", "prompts", "user_experto_3.txt")
+    prompt_path =  os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'prompts', 'experto_dastage_extraccion.txt'))
     with open(prompt_path, "r", encoding="utf-8") as f:
         prompt_template = f.read()
     # prompt = prompt_template.format(technical_explanation=technical_explanation, nombre_proceso_ppal=nombre_proceso_ppal)
@@ -135,6 +135,7 @@ def odi_migration_team(xml_input_folder):
         print("=========================================")
         print(user_explanation_text)
         print("=========================================")
+        return user_explanation_text
         # print("\n¿Apruebas esta migración? Si no estás de acuerdo, escribe tus comentarios")
         # respuesta = input("> ").strip().lower()
         # if respuesta == "si":
