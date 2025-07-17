973664497ed29a8b73a8629749ded466114fa871
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-07-17 10:39:46 -0500

    prueba con sql

--- app/utilities/team_manager.py
+++ app/utilities/team_manager.py
@@ -8,6 +8,7 @@ from typing import List
 def save_base64_files(file_items: List[dict], upload_dir: str) -> List[str]:
     os.makedirs(upload_dir, exist_ok=True)
     for item in file_items:
+        print(item.file)
         file_content = base64.b64decode(item.file)
         filename = item.fileName
         file_path = os.path.join(upload_dir, filename)
@@ -19,9 +20,10 @@ def team_manager(request):
     team_id = request.agent_id
     
     if team_id == "team_join_sql":
-        upload_dir = "./tmp/sql_inputs"
-        save_base64_files(request.files, upload_dir)
-        return join_sql_scripts_team(upload_dir)
+        # upload_dir = "./tmp/sql_inputs"
+        # save_base64_files(request.files, upload_dir)
+        # return join_sql_scripts_team(upload_dir)
+        return "team_join_sql"
     
     elif team_id == "team_odi_migration":
         upload_dir = os.path.join(os.path.dirname(__file__), "..", "..", "tmp", "odi_inputs")
