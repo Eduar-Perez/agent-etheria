39321df6e053935d73d507b66073f40faa868974
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-07-21 16:09:19 -0500

    correcciond e erorr en load_process

--- app/agents/teams/odi_migration/utilities/dtsge_agent_descripcion_tarea.py
+++ app/agents/teams/odi_migration/utilities/dtsge_agent_descripcion_tarea.py
@@ -279,8 +279,8 @@ def describir_tarea_datastage(state: DescripcionState) -> DescripcionState:
 def exportar_por_llave_descripcion_state(state: DescripcionState) -> DescripcionState:
     nombre_proceso_ppal = state["nombre_proceso_ppal"]
     # ruta = os.path.join("prompts_datastage", nombre_proceso_ppal)
-    base_dir = os.path.dirname(__file__)
-    ruta = os.path.join(base_dir, "tmp", "prompts_datastage", nombre_proceso_ppal)
+    base_tmp = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "tmp"))
+    ruta = os.path.join(base_tmp, "prompts_datastage", nombre_proceso_ppal)
     if not os.path.exists(ruta):
         os.makedirs(ruta, exist_ok=True)
     archivos = [f for f in os.listdir(ruta) if f.endswith(".json")]
