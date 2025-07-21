b32309fa7d2d5c52ef8a13327aeb3a85e506d307
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-07-21 12:12:45 -0500

    se elimina impresión del body de entrada

--- app/api/routes.py
+++ app/api/routes.py
@@ -20,7 +20,7 @@ def configure_routes(app: FastAPI):
     async def log_raw_request(request: Request, call_next):
         body = await request.body()
         logger.info("Request Body: %s", body.decode("utf-8"))
-        print("body", body)
+        # print("body", body)
         return await call_next(request)
     @app.post("/task")
     async def ask_question(request: QuestionsRequest):
