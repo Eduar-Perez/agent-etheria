67f9a198476c1dfd6001efa53436e2d92af03bd4
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-07-23 16:44:26 -0500

    generar url

--- app/agents/teams/utilities/test_save.py
+++ app/agents/teams/utilities/test_save.py
@@ -3,7 +3,7 @@ from save_and_generate_url import save_and_generate_url_s3
 def main():
     bucket_name = "agents-temp-files"
     s3_key = "sql-joiner/migracion_odi_a_datastage_2025-07-21.xml"
-    file_path = "../../../../temporal_files/DM_CHQGEREN_002 - Cheques de Gerencia Pendientes de Pago.sql"
+    file_path = "../../../../temporal_files/DM_CHQGEREN_010 - CHEQUES DE GERENCIA MOVIMIENTOS DÍA.sql"
     url = save_and_generate_url_s3(bucket_name, s3_key, file_path)
     return url
 
