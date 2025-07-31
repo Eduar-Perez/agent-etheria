525e45dfce5219d5dcc3895ea285e8f1cc531eac
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-07-31 17:18:42 -0500

    cambios en el headder

--- app/agents/teams/__init__.py
+++ app/agents/teams/__init__.py
@@ -1,6 +1,6 @@
 from .sql_joiner.sql_join import join_sql_scripts_team
 from .odi_migration.odi_migration_team import odi_migration_team
 from .bus_migration.bus_migration import bus_migration_team
-from .datastage_documentation import generate_documentation_datastage
+from .datastage_documentation.generate_documentation_datastage import generate_document_datastage
 
-__all__ = ["join_sql_scripts_team", "odi_migration_team", "bus_migration_team","generate_documentation_datastage"]
+__all__ = ["join_sql_scripts_team", "odi_migration_team", "bus_migration_team","generate_document_datastage"]
