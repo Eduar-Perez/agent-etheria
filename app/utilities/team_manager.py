9041c4895e2c910ef4793d7f36465fc86fe88cdc
Daniel Zambrano <danielzambranoa@javeriana.edu.co>
2025-08-08 16:12:13 -0500

    imprimir

--- app/utilities/team_manager.py
+++ app/utilities/team_manager.py
@@ -16,6 +16,7 @@ def save_base64_files(file_items: List[dict], upload_dir: str) -> List[str]:
     if len(file_items) < 1:
         return "No se recibieron los archivos para procesar"
     for item in file_items:
+        print("Este es el item",item)
         print(item.file.split(",")[1])
         file_content = base64.b64decode(item.file.split(",")[1])
         filename = item.fileName
